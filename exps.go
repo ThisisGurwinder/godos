5f02105cd3bb07f3e5df636aab069cbba01ecc89
Gurwinder <ThisisGurwinder@Gmail.com>
2019-04-02 23:17:32 +0530

    Update exps.go

--- exps.go
+++ exps.go
@@ -4,6 +4,6 @@ const (
 	ISSUE_URL_REGEX = "\\[(Issue:[^\\]]*)\\]"
 	TODO_REGEX      = "TODO(\\([^)]+\\))?:(.*)"
 	COMMENT_REGEX   = "NOTE(\\([^)]+\\))?:(.*)"
-	ISSUE_BODY      = "On file: [%s](%s) %s"
+	ISSUE_BODY      = "On file: [%s](%s) \n %s"
 	GITHUB_FILE_URL = "https://github.com/%s/%s/blob/%s/%s"
 )
