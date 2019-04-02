59f615c71cd14e49b9f6e41cc0f9444fc94da719
Gurwinder <ThisisGurwinder@Gmail.com>
2019-04-02 22:34:07 +0530

    Update exps.go

--- exps.go
+++ exps.go
@@ -3,7 +3,7 @@ package main
 const (
 	ISSUE_URL_REGEX = "\\[(Issue:[^\\]]*)\\]"
 	TODO_REGEX      = "TODO(\\([^)]+\\))?:(.*)"
-	COMMENT_REGEX   = "\\[(::[^\\]]*)\\]"
+	COMMENT_REGEX   = "FOLLOWUP(\\([^)]+\\))?:(.*)"
 	ISSUE_BODY      = "On file: [%s](%s) %s"
 	GITHUB_FILE_URL = "https://github.com/%s/%s/blob/%s/%s"
 )
